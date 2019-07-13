c8814bbfb9533ea257230ede5375655a4a100500
Ravi Sharma <ravisharma1987@gmail.com>
2019-07-13 17:00:02 +0200

    Created new project for up42 coding challenge

---
+++ src/main/java/com/up42/coding/challenge/Up42CcApplication.java
@@ -0,0 +1,13 @@
+package com.up42.coding.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Up42CcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Up42CcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/up42/coding/challenge/Up42CcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.up42.coding.challenge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Up42CcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
